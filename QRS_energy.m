947d493ee4591500cfb2c5f1a2da3d326e668696
aleksandrarak <aleksandrarak93@gmail.com>
2016-12-20 23:36:33 +0100

    jj

--- QRS_energy.m
+++ QRS_energy.m
@@ -2,7 +2,7 @@ function [ QRS_energy ] = QRS_energy( signal, QRS_start, QRS_end )
 
 % funkcja oblicza energie zespolu QRS
 
-QRS_energy = sum((signal(QRS_start:QRS_end)).^2)
+QRS_energy = sum((signal(QRS_start:QRS_end)).^2);
 
 
 end
